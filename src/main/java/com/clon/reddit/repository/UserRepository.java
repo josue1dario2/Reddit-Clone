3ee7b8e9ec34793f4f2f96688a2ded35c494b049
Josue Solis <josue1dario2solis3@gmail.com>
2022-07-11 23:40:46 -0300

    Create User repository

--- src/main/java/com/clon/reddit/repository/UserRepository.java
+++ src/main/java/com/clon/reddit/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.clon.reddit.repository;public interface UserRepository {
+package com.clon.reddit.repository;
+
+import com.clon.reddit.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
 }
