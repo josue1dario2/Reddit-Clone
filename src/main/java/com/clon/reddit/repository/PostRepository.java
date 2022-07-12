a079cdaa1d53fad6409831e092aa9ca9c152deb4
Josue Solis <josue1dario2solis3@gmail.com>
2022-07-11 23:37:56 -0300

    Create Post repository

--- src/main/java/com/clon/reddit/repository/PostRepository.java
+++ src/main/java/com/clon/reddit/repository/PostRepository.java
@@ -1,2 +1,9 @@
-package com.clon.reddit.repository;public interface PostRepository {
+package com.clon.reddit.repository;
+
+import com.clon.reddit.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Integer> {
 }
