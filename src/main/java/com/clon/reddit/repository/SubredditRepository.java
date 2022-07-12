50a00d7aaef03fe01428240d884eac3cdea246fe
Josue Solis <josue1dario2solis3@gmail.com>
2022-07-11 23:39:03 -0300

    Create Subreddit repository

--- src/main/java/com/clon/reddit/repository/SubredditRepository.java
+++ src/main/java/com/clon/reddit/repository/SubredditRepository.java
@@ -1,2 +1,9 @@
-package com.clon.reddit.repository;public interface SubredditRepository {
+package com.clon.reddit.repository;
+
+import com.clon.reddit.model.Subreddit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubredditRepository extends JpaRepository<Subreddit,Integer> {
 }
