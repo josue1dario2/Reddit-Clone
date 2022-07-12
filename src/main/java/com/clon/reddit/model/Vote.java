fdaeccb5f570696ea022b98f1991a949a1c40a1f
Josue Solis <josue1dario2solis3@gmail.com>
2022-07-11 23:17:46 -0300

    Add entity Vote

--- src/main/java/com/clon/reddit/model/Vote.java
+++ src/main/java/com/clon/reddit/model/Vote.java
@@ -1,4 +1,29 @@
 package com.clon.reddit.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
 public class Vote {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer voteId;
+    private VoteType voteType;
+    @NotNull
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
 }
