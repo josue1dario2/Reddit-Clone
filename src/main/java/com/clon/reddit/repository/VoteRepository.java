927c9f0287626472ecf60ebbb781eeb869b77202
Josue Solis <josue1dario2solis3@gmail.com>
2022-07-11 23:41:36 -0300

    Create Vote repository

--- src/main/java/com/clon/reddit/repository/VoteRepository.java
+++ src/main/java/com/clon/reddit/repository/VoteRepository.java
@@ -1,2 +1,9 @@
-package com.clon.reddit.repository;public interface VoteRepository {
+package com.clon.reddit.repository;
+
+import com.clon.reddit.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote,Integer> {
 }
