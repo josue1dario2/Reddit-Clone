c139a9b3dde7c17a66acf5183e164402fb9fd76f
Josue Solis <josue1dario2solis3@gmail.com>
2022-07-11 22:59:50 -0300

    Create entity class User

--- src/main/java/com/clon/reddit/model/User.java
+++ src/main/java/com/clon/reddit/model/User.java
@@ -1,2 +1,37 @@
-package com.clon.reddit.model;public class User {
+package com.clon.reddit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+import java.time.Instant;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer userId;
+    @NotBlank(message = "Username is required")
+    private String username;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @Email
+    @NotEmpty(message = "Email is required")
+    private String email;
+    private Instant created;
+    private boolean enabled;
 }
